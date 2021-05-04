5a14b72feaa8fe683c86d5c1e7e6e4e28b36a001
Elmin Nurkic <nurkicelmin@hotmail.com>
2021-05-04 21:36:32 +0200

    Added ejs and partials, partials makes our code cleaner. Added Css in head.

--- app.js
+++ app.js
@@ -14,9 +14,14 @@ app.listen(3000);
 app.get("/", (req, res) => {
   //sets automaticly contnet type header
   //res.sendFile("./views/index.html", { root: __dirname });
-
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  
   //need to render the view, when using ejs
-  res.render('index')
+  res.render('index', {title: 'Home', blogs})
 });
 
 //setting up routes
@@ -27,18 +32,25 @@ app.get("/about", (req, res) => {
   // send a ahtml file , we neeed to use __dirname to have correct path
   //res.sendFile("./views/about.html", { root: __dirname });
 
-  res.render('about')
+  
+
+  res.render('about', {title: 'About'})
 });
 
 //redirects
-app.get("/about-us", (req, res) => {
+/* app.get("/about-us", (req, res) => {
   res.redirect("/about");
-});
+}); */
+
+//create a blog post
+app.get('/blogs/create', (req, res) => {
+    res.render('create', {title: 'Create'})
+})
 
 //404 page, use is middlewere - must be in bottom of page- because it's not scooped
 app.use((req, res) => {
   // this function fire of if the rest is not matched
 //   res.status(404).sendFile("./views/404.html", { root: __dirname });
 
-    res.status(404).render('404');
+    res.status(404).render('404', {title: '404'});
 });
