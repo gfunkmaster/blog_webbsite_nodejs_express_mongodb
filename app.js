f9e662d125538cf654ee26d7269d11512fc4735e
Elmin Nurkic <nurkicelmin@hotmail.com>
2021-05-04 15:45:28 +0200

    using esj, we use render instead of sendfile, created ejs sites

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ app.listen(3000);
 //takes in two argument, first url. second is a function takes in req and res
 app.get("/", (req, res) => {
   //sets automaticly contnet type header
-  res.sendFile("./views/index.html", { root: __dirname });
+  //res.sendFile("./views/index.html", { root: __dirname });
+
+  //need to render the view, when using ejs
+  res.render('index')
 });
 
 //setting up routes
@@ -22,7 +25,9 @@ app.get("/about", (req, res) => {
   //sets automaticly contnet type header
   // res.send('./views/about.html')
   // send a ahtml file , we neeed to use __dirname to have correct path
-  res.sendFile("./views/about.html", { root: __dirname });
+  //res.sendFile("./views/about.html", { root: __dirname });
+
+  res.render('about')
 });
 
 //redirects
@@ -33,5 +38,7 @@ app.get("/about-us", (req, res) => {
 //404 page, use is middlewere - must be in bottom of page- because it's not scooped
 app.use((req, res) => {
   // this function fire of if the rest is not matched
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+//   res.status(404).sendFile("./views/404.html", { root: __dirname });
+
+    res.status(404).render('404');
 });
